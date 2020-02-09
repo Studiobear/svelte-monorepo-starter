baefe50dfa313572ba74907d4a230fb6f89dfa1f
Barry Low <bearcanrun@gmail.com>
2020-02-09 14:25:15 -0800

    bug: fix global jest config

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,18 @@
 module.exports = {
   verbose: true,
   transform: {
-    "^.+\\.svelte$": "svelte-jester"
+    '^.+\\.svelte$': 'svelte-jester',
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.([tj]sx?)$',
-  "moduleFileExtensions": ["js", "svelte"]
-  testPathIgnorePatterns: ['node_modules', 'cypress', '.cache', 'public', 'dist', 'dist-esm'],
+  moduleFileExtensions: ['js', 'svelte'],
+  testPathIgnorePatterns: [
+    'node_modules',
+    'cypress',
+    '.cache',
+    'public',
+    'dist',
+    'dist-esm',
+  ],
   transformIgnorePatterns: [
     'node_modules/(?!(gatsby|@ionic|ionicons)/)',
     '<rootDir>/node_modules/(?!(gatsby|@ionic|ionicons)/)',
