99c755d8ff370ea0b7cec9d0fc68acc443bbeba9
Barry Low <bearcanrun@gmail.com>
2020-02-13 17:44:29 -0800

    fix(my-site): extract frontmatter from md

--- apps/my-site/src/utils/__tests__/md-processor.test.js
+++ apps/my-site/src/utils/__tests__/md-processor.test.js
@@ -1,10 +1,11 @@
 import vfile from 'vfile'
-import { processor, toml2data } from '../md-processor'
+import { processor } from '../md-processor'
 
 describe('MD Processor', () => {
   test('should post object', async () => {
     const post = await processor
-
+    console.log(post)
     expect(post).toHaveProperty('title')
+    expect(post).toHaveProperty('slug')
   })
 })
--- apps/my-site/src/utils/md-processor.js
+++ apps/my-site/src/utils/md-processor.js
@@ -1,100 +1,45 @@
-import vfile from 'vfile'
+// import vfile from 'vfile'
 import toVfile from 'to-vfile'
 import VMessage from 'vfile-message'
 import report from 'vfile-reporter'
 import unified from 'unified'
-import remark from 'remark'
+// import remark from 'remark'
 import markdown from 'remark-parse'
-import slug from 'remark-slug'
-import toc from 'remark-toc'
+// import slug from 'remark-slug'
+// import toc from 'remark-toc'
 import frontmatter from 'remark-frontmatter'
-import remark2retext from 'remark-retext'
-import english from 'retext-english'
-import indefiniteArticle from 'retext-indefinite-article'
+import yaml from 'yaml'
+import extractFM from 'remark-extract-frontmatter'
+// import remark2retext from 'remark-retext'
+// import english from 'retext-english'
+// import indefiniteArticle from 'retext-indefinite-article'
 import remark2rehype from 'remark-rehype'
-import doc from 'rehype-document'
-import format from 'rehype-format'
+// import doc from 'rehype-document'
+// import format from 'rehype-format'
 import stringify from 'rehype-stringify'
 import path from 'path'
 
-export const md = `
----
-title: "Testing Markdown"
----
-
-# Test Title
-
-## Subtitle Testing
-
-This is really just a test
-`
-/*
 export const processor = unified()
   .use(markdown)
-  .use(log)
-  .use(parse_yaml)
-  .use(add)
   .use(remark2rehype)
   .use(frontmatter)
-  .use(log)
+  .use(extractFM, { name: 'frontmatter', yaml: yaml.parse })
   .use(stringify)
-*/
-
-export const processor = unified()
-  .use(markdown)
-  .use(frontmatter, ['yaml', 'toml'])
-  .use(yaml2Data)
-  .use(remark2rehype)
-  .use(stringify)
-  .use(yamlParse)
   .use(log)
-  .process(toVfile.readSync('./src/utils/example.md', 'utf8'), function(
-    err,
-    file,
-  ) {
-    console.log(file)
-    console.error(report(err || file))
-  })
-
-//processor.process(md).then(x => console.log(x))
-
-function yaml2Data() {
-  const transformer = (tree, vFile) => {
-    vFile.data.yaml = tree.children[0].value
-    console.log('yaml2Data:', tree, vFile)
-  }
-  return transformer
-}
-
-function yamlParse() {
-  const parser = (tree, vFile) => {
-    const yamlString = vFile.data.yaml
-    const yamlSpace = yamlString.replace(/([^"]+)|("[^"]+")/g, function(
-      $0,
-      $1,
-      $2,
-    ) {
-      if ($1) {
-        return $1.replace(/\s/g, '')
-      } else {
-        return $2.replace(/"/g, '')
+  .process(toVfile.readSync('./src/utils/example.md', 'utf8'))
+  .then(
+    function(file) {
+      return {
+        title: file.data.frontmatter.title || 'No title',
+        slug: file.data.frontmatter.slug || '',
+        html: file.contents,
       }
-    })
-    const yaml = yamlSpace.split('=')
-    vFile.data.frontmatter = {}
-    vFile.data.frontmatter[yaml[0]] = yaml[1]
-    console.log('yamlParse:', tree, vFile)
-  }
-  return parser
-}
-
-function add() {
-  return patch
-
-  function patch(_, file) {
-    file.data.message = 'Hi!'
-  }
-}
+    },
+    function(err) {
+      throw new Error(err)
+    },
+  )
+  .catch(console.error)
 
 function log() {
   const logger = (tree, file) => {
