a2683b9ad6a4515f2e05a2a8fb5913ffe980546e
Barry Low <bearcanrun@gmail.com>
2020-02-13 23:27:37 -0800

    feat(my-site): generate excerpts using excerpt comment in md

--- apps/my-site/src/utils/example.md
+++ apps/my-site/src/utils/example.md
@@ -4,3 +4,9 @@ slug: new-website
 ---
 
 # Other markdown
+
+This is the intro and used for an excerpt.
+
+<!-- excerpt -->
+
+The rest of this is part of the main content.
--- apps/my-site/src/utils/md-processor.js
+++ apps/my-site/src/utils/md-processor.js
@@ -1,38 +1,31 @@
-// import vfile from 'vfile'
 import toVfile from 'to-vfile'
 import VMessage from 'vfile-message'
 import report from 'vfile-reporter'
 import unified from 'unified'
-// import remark from 'remark'
 import markdown from 'remark-parse'
 // import slug from 'remark-slug'
 // import toc from 'remark-toc'
 import frontmatter from 'remark-frontmatter'
 import yaml from 'yaml'
 import extractFM from 'remark-extract-frontmatter'
+import excerpt from 'remark-excerpt'
 // import remark2retext from 'remark-retext'
 // import english from 'retext-english'
 // import indefiniteArticle from 'retext-indefinite-article'
 import remark2rehype from 'remark-rehype'
-// import doc from 'rehype-document'
 // import format from 'rehype-format'
 import stringify from 'rehype-stringify'
 import path from 'path'
 
-export const processMD = filepath => (foo = 'bar') => {
-  console.log('processMD: ', filepath, foo)
-  return filepath
-}
-
-export const processor = filepath => async () =>
-  await unified()
+export const processor = filepath => async () => {
+  const postBody = await unified()
     .use(markdown)
     .use(remark2rehype)
     .use(frontmatter)
     .use(extractFM, { name: 'frontmatter', yaml: yaml.parse })
     .use(rmfm)
     .use(stringify)
-    .use(log)
+    // .use(log)
     .process(toVfile.readSync(filepath, 'utf8'))
     .then(file => ({
       title: file.data.frontmatter.title || 'No title',
@@ -40,6 +33,22 @@ export const processor = filepath => async () =>
       html: file.contents,
     }))
     .catch(console.error)
+
+  const postExcerpt = await unified()
+    .use(markdown)
+    .use(excerpt)
+    .use(remark2rehype)
+    .use(frontmatter)
+    .use(stringify)
+    // .use(log)
+    .process(toVfile.readSync(filepath, 'utf8'))
+    .then(file => ({
+      excerpt: file.contents,
+    }))
+    .catch(console.error)
+
+  return Object.assign({}, postBody, postExcerpt)
+}
 
 const rmfm = removeFrontmatter
 
